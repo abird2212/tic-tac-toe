c68d0bc6f986932368b5a4693c39b54d2d0630d5
abird <abird@newroads.org>
2017-02-08 12:35:17 -0500

    Add check for winner

--- Board.java
+++ Board.java
@@ -6,9 +6,9 @@ public class Board{
                            {' ', ' ', ' '}};
 
 
-public Board(){
-   
-}
+  public Board(){
+
+  }
   
   public void makeMove(char symbol, int row, int col){
     board[row][col] = symbol;
@@ -17,21 +17,41 @@ public Board(){
 
   public void drawBoard(){
     
-    System.out.println(" " + board[0][0] + " | " +  board[0][1] + " | " + board[0][2] + " ");
-    System.out.println("-----------");
-    System.out.println(" " + board[1][0] + " | " +  board[1][1] + " | " + board[1][2] + " ");
-    System.out.println("-----------");
-    System.out.println(" " + board[2][0] + " | " +  board[2][1] + " | " + board[2][2] + " ");
+    System.out.println("                                                                             ");
+    System.out.println("            " + board[0][0] + " | " +  board[0][1] + " | " + board[0][2] + " ");
+    System.out.println("           -----------");
+    System.out.println("            " + board[1][0] + " | " +  board[1][1] + " | " + board[1][2] + " ");
+    System.out.println("           -----------");
+    System.out.println("            " + board[2][0] + " | " +  board[2][1] + " | " + board[2][2] + " ");
+    System.out.println("                                                                             ");
+ 
   }
+
   
+  // <----------> side to side:
   
   public boolean TTTChecker(){
-    if(board[0][0] == board[0][1] == board[0][1]){
+    if((board[0][0] == board[0][1]) && (board[0][1] == board[0][2]))
       return true;
-    }
-    else{
+    else if ((board[1][0] == board[1][1]) && (board[1][1] == board[1][2]))
+      return true;
+    else if((board[2][0] == board[2][1]) && (board[2][1] == board[2][2]))
+      return true;
+    else if((board[0][0] == board[1][0]) && (board[0][0] == board[2][0]))
+      return true;    
+    else if((board[0][1] == board[1][1]) && (board[1][1] == board[2][1]))
+      return true;    
+    else if((board[0][2] == board[1][2]) && (board[0][2] == board[2][2]))
+      return true;  
+    else if((board[0][0] == board[1][1]) && (board[0][0] == board[2][2]))
+      return true;  
+    else if((board[0][2] == board[1][1]) && (board[0][2] == board[2][0]))
+      return true;     
+    else
       return false;
+    
     }
-  }
-  
+
 }
+  
+  
--- PlayTTT_2.java
+++ PlayTTT_2.java
@@ -6,26 +6,32 @@ public class PlayTTT_2{
 public static void main(String[] args){
   
 
-  System.out.println ( "                                     ");
-  System.out.println ("                      _______________ ");
-  System.out.println ("                      | How to play | ");
-  System.out.println ("                      | Tic Tac Toe | ");
-  System.out.println ("                       ------------- " );
-  System.out.println ( "                                     ");
-  System.out.println ("  1. Give your names. (player 1 is X and 2 is o) ");
-  System.out.println ( "                                     ");
-   
+  
+  
+  
+  
+  System.out.println ("                                                ");
+  System.out.println ("                             _______________    ");
+  System.out.println ("                             | How to play |    ");
+  System.out.println ("                             | Tic Tac Toe |    ");
+  System.out.println ("                              -------------     ");
+  System.out.println ("                                                     ");
+  System.out.println ("      1. Give your names. (player 1 is X and 2 is O) ");
+  System.out.println ("                                                     ");
+     
   
   
   Scanner in = new Scanner(System.in);
   
   System.out.println("Whats your name Player 1: ");
-    
+  System.out.println ( "                                         ");
+  
     String nameP1 = in.next();
     
     Player player1 = new Player('x', false, nameP1);
     
     System.out.println("Player 2 give a name: ");
+    System.out.println ( "                                         ");
     
     String nameP2 = in.next();
     
@@ -33,37 +39,54 @@ public static void main(String[] args){
     
   
 
-    
-   System.out.println (" 2. player 1 goes first picking a place to put their x");
-    System.out.println ("3. player 2 goes fsecond picking a place to put their o");
-    System.out.println ("4. this continues until player 1 or 2 gets three of their variebules in a straight line");
+    System.out.println ( "                                                                                                    ");
+    System.out.println ( "     2. player 1 goes first picking a place to put their x                                          ");
+    System.out.println ( "                                                                                                    ");
+    System.out.println ( "     3. player 2 goes fsecond picking a place to put their o                                        ");
+    System.out.println ( "                                                                                                    ");
+    System.out.println ( "     4. This process continues until player 1 or 2 gets three of their variebules in a straight line");
+    System.out.println ( "                                                                                                    ");
+    System.out.println ( "     5. Things to know:                                                                             ");
+    System.out.println ( "   - Do not enter the same #'s as the other player!                                                 ");
+    System.out.println ( "   - If someone enters something other than a # the game will turn off!                             ");
+    System.out.println ( "   - This is a work in progress game!                                                               ");
+    System.out.println ( "                                                                                                    ");
+    System.out.println ( "                                                                                                    ");
   
-    System.out.println ( "                                   ");
-    System.out.println ( "   0       1       2               ");
-    System.out.println ( "   |       |       |               ");
-    System.out.println ( "   v       v       v               ");
-    System.out.println ( "                                   ");
-    System.out.println ( "       |        |                  ");
-    System.out.println ( "   00  |   01   | 02       <----- 0");
-    System.out.println ( " ----------------------            ");
-    System.out.println ( "       |        |                  ");
-    System.out.println ( "   10  |   11   | 12       <----- 1");
-    System.out.println ( " ----------------------            ");
-    System.out.println ( "       |        |                  ");
-    System.out.println ( "   20  |   21   | 22       <----- 2");
-    System.out.println ( "                                   ");
+    System.out.println ( "                                         ");
+    System.out.println ( "         0       1       2               ");
+    System.out.println ( "         |       |       |               ");
+    System.out.println ( "         v       v       v               ");
+    System.out.println ( "                                         ");
+    System.out.println ( "             |        |                  ");
+    System.out.println ( "         00  |   01   | 02       <----- 0");
+    System.out.println ( "       ----------------------            ");
+    System.out.println ( "             |        |                  ");
+    System.out.println ( "         10  |   11   | 12       <----- 1");
+    System.out.println ( "       ----------------------            ");
+    System.out.println ( "             |        |                  ");
+    System.out.println ( "         20  |   21   | 22       <----- 2");
+    System.out.println ( "                                         ");
+    System.out.println ( "                                         ");
     
   
     Board board = new Board();
     
 
-    System.out.println("Player " + player1.getName() + ", give row and then column: ");
-    int row = in.nextInt();
-    int col = in.nextInt();
-    
-    if()
-    board.makeMove(player1.getSymbol(), row, col);
-    board.drawBoard();
+    while(!TTTChecker()){
+      System.out.println("Player " + player1.getName() + ", give row and then column: ");
+      int row = in.nextInt();
+      int col = in.nextInt();
+
+      board.makeMove(player1.getSymbol(), row, col);
+        board.drawBoard();
+
+      System.out.println("Player " + player2.getName() + ", give row and then column: ");
+      row = in.nextInt();
+      col = in.nextInt();
+    }
+   
+    }
   }
-}
+
     
