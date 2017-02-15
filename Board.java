162727d6305ef41237bb72fe86b78066357f3735
abird <abird@newroads.org>
2017-02-15 12:29:37 -0500

    lets goo

--- Board.java
+++ Board.java
@@ -31,21 +31,21 @@ public class Board{
   // <----------> side to side:
   
   public boolean TTTChecker(){
-    if((board[0][0] == board[0][1]) && (board[0][1] == board[0][2]))
+    if((board[0][0] == board[0][1]) && (board[0][1] == board[0][2]) && board[0][0] =="")
       return true;
-    else if ((board[1][0] == board[1][1]) && (board[1][1] == board[1][2]))
+    else if ((board[1][0] == board[1][1]) && (board[1][1] == board[1][2]) && board[1][0] =="")
       return true;
-    else if((board[2][0] == board[2][1]) && (board[2][1] == board[2][2]))
+    else if((board[2][0] == board[2][1]) && (board[2][1] == board[2][2]) && board[2][0] =="")
       return true;
-    else if((board[0][0] == board[1][0]) && (board[0][0] == board[2][0]))
+    else if((board[0][0] == board[1][0]) && (board[0][0] == board[2][0]) && board[0][0] =="")
       return true;    
-    else if((board[0][1] == board[1][1]) && (board[1][1] == board[2][1]))
+    else if((board[0][1] == board[1][1]) && (board[1][1] == board[2][1]) && board[0][1] =="")
       return true;    
-    else if((board[0][2] == board[1][2]) && (board[0][2] == board[2][2]))
+    else if((board[0][2] == board[1][2]) && (board[0][2] == board[2][2]) && board[0][2] =="")
       return true;  
-    else if((board[0][0] == board[1][1]) && (board[0][0] == board[2][2]))
+    else if((board[0][0] == board[1][1]) && (board[0][0] == board[2][2]) && board[0][0] =="")
       return true;  
-    else if((board[0][2] == board[1][1]) && (board[0][2] == board[2][0]))
+    else if((board[0][2] == board[1][1]) && (board[0][2] == board[2][0]) && board[0][2] =="")
       return true;     
     else
       return false;
